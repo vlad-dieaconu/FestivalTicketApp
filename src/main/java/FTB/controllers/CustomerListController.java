27aa98f9258d98a3f632ca5db0ac964a0bbb4282
vlad-dieaconu <vladdieaconu@yahoo.com>
2020-05-14 15:32:13 +0300

    Added the method which change the scene back to admin view

--- src/main/java/FTB/controllers/CustomerListController.java
+++ src/main/java/FTB/controllers/CustomerListController.java
@@ -4,8 +4,14 @@ package FTB.controllers;
 import FTB.model.User;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 
 import java.io.IOException;
@@ -40,6 +46,16 @@ public class CustomerListController {
         users = objectMapper.readValue(USERS_PATH.toFile(),
                 new TypeReference<List<User>>() {
                 });
+    }
+
+    public void backToAdminView(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("adminView.fxml"));
+        Scene view2 = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(view2);
+        window.show();
     }
 
 
