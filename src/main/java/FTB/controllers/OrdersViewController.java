a9ae6b783e588875e00fa7f5391616cedcc740a4
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2020-05-17 18:24:34 +0300

    Temporary view for user's orders.

---
+++ src/main/java/FTB/controllers/OrdersViewController.java
@@ -0,0 +1,24 @@
+package FTB.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OrdersViewController {
+
+    public void backToUser(ActionEvent event) throws IOException {
+
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("userView.fxml"));
+        Scene view2 = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(view2);
+        window.show();
+    }
+}
