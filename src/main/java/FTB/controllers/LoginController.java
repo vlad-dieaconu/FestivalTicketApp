e9ed397ba20fa748ff6b927d4fa30f6211b4ff94
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2020-06-18 19:36:19 +0300

    Added the method for close window.

--- src/main/java/FTB/controllers/LoginController.java
+++ src/main/java/FTB/controllers/LoginController.java
@@ -100,5 +100,15 @@ public class LoginController {
         return user;
     }
 
+    public void minimizeWindow(javafx.event.ActionEvent min) {
+        Stage window = (Stage) ((Node)min.getSource()).getScene().getWindow();
+        window.setIconified(true);
+    }
+
+    public void closeWindow(javafx.event.ActionEvent close) {
+        Stage window = (Stage) ((Node)close.getSource()).getScene().getWindow();
+        window.close();
+    }
+
 
 }
