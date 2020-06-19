edb94b1ebf7ce2736a51808a24b88a825047483b
vlad-dieaconu <vladdieaconu@yahoo.com>
2020-06-19 21:23:14 +0300

    Tests for user services.

---
+++ src/test/java/FTB/services/UserServiceTest.java
@@ -0,0 +1,99 @@
+package FTB.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import FTB.exceptions.UsernameAlreadyExistsException;
+import FTB.model.User;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @BeforeClass
+    public static void setupClass() {
+        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
+        FileSystemService.initApplicationHomeDirIfNeeded();
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
+    }
+
+    @Test
+    public void testCopyDefaultFileIfNotExists() throws Exception {
+        UserService.loadUsersFromFile();
+        assertTrue(Files.exists(UserService.USERS_PATH));
+    }
+
+    @Test
+    public void testLoadUsersFromFile() throws Exception {
+        UserService.loadUsersFromFile();
+        assertNotNull(UserService.users);
+        assertEquals(0, UserService.users.size());
+    }
+
+    @Test
+    public void testAddOneUser() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test0", "testPass0", "test@email.com","20");
+        assertNotNull(UserService.users);
+        assertEquals(1, UserService.users.size());
+    }
+
+    @Test
+    public void testAddTwoUsers() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test1", "testPass1", "test1@email.com","30");
+        UserService.addUser("test2", "testPass2", "test2@email.com","40");
+        assertNotNull(UserService.users);
+        assertEquals(2, UserService.users.size());
+    }
+
+    @Test(expected = UsernameAlreadyExistsException.class)
+    public void testAddUserAlreadyExists() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test1", "testPass3", "test@mail.com","20");
+        assertNotNull(UserService.users);
+        UserService.checkUserDoesNotAlreadyExist("test1");
+    }
+
+    @Test
+    public void testAddOneUserIsPersisted() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test9", "testPass9", "123@email.com","30");
+        List<User> users = new ObjectMapper().readValue(UserService.USERS_PATH.toFile(), new TypeReference<List<User>>() {
+        });
+        assertNotNull(users);
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    public void testAddTwoUserArePersisted() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test11", "testPass11", "email1@gmail.com","30");
+        UserService.addUser("test122", "testPass12", "email2@gmail.com","24");
+        List<User> users = new ObjectMapper().readValue(UserService.USERS_PATH.toFile(), new TypeReference<List<User>>() {
+        });
+        assertNotNull(users);
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    public void testPasswordEncoding() {
+        assertNotEquals("testPass1", UserService.encodePassword("username1", "testPass1"));
+    }
+
+    @Test
+    public void testCheckUser(){
+
+    }
+}
